7ced9757fe9054e7f718ca4f2b493c9a280b50c4
thehatepta <thehatepta@gmail.com>
2019-03-28 17:09:47 +0200

    redirect implemented

---
+++ src/main/java/com/urlShorter/controllers/services/RandomCharCreators.java
@@ -0,0 +1,13 @@
+package com.urlShorter.controllers.services;
+
+public class RandomCharCreators {
+
+    public static String getRandomChars() {
+        String randomStr = "";
+        String possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (int i = 0; i < 5; i++)
+            randomStr += possibleChars.charAt((int) Math.floor(Math.random() * possibleChars.length()));
+        return randomStr;
+    }
+
+}
--- src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class main {
-    public static void main(String[] args) {
-        SpringApplication.run(main.class, args);
-    }
-}
